222628dcb3fd09845b9ff64f3f2a82862d0cf1a4
Fink2005 <phans3806@gmail.com>
2025-07-10 21:54:33 +0700

    delete

--- src/app/apis/requests/connectWallet.ts
+++
@@ -1,8 +0,0 @@
-import apiRequest from '@/app/apis/apiRequest';
-
-const connectWalletRequest = {
-  async getNonce(value: string): Promise<{ nonce: string } | null> {
-    return await apiRequest<{ nonce: string }>(`/auth/nonce/${value}`, 'GET');
-  }
-};
-export default connectWalletRequest;

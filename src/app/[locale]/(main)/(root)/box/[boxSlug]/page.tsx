d18ae8043647d0c5da227bb4aa576fbab73bfd66
Fink2005 <phans3806@gmail.com>
2025-07-26 04:51:07 +0700

    Revert "fix: build"
    This reverts commit 93f47e77d7b46746eb05ef5498f37a7684194ecb.

--- src/app/[locale]/(main)/(root)/box/[boxSlug]/page.tsx
+++ src/app/[locale]/(main)/(root)/box/[boxSlug]/page.tsx
@@ -11,13 +11,14 @@ type PageProps = {
 };
 
 const page = async ({ params }: PageProps) => {
-  const { boxSlug } = await params;
+  const { boxSlug, locale } = await params;
 
   return (
     <div className="min-h-screen bg-9x9 flex flex-col items-center pt-10 px-3 sm:px-7">
       <Link href="/">
         <LeftArrowIcon className="absolute left-4" />
       </Link>
+      <span className="hidden">{locale}</span>
       <div className="flex flex-col items-center mb-6">
         <h1 className="text-shadow-custom text-[1.25rem] font-[700]">Box 1 - Sao thức tỉnh</h1>
         <h2 className="text-shadow-custom text-[0.75rem] font-[400] text-center">

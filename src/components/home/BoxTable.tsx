113f6d69589b1a24e71be06a842611987ebfe869
Fink2005 <phans3806@gmail.com>
2025-08-09 10:21:18 +0700

    feat:change hop to box

--- src/components/home/BoxTable.tsx
+++ src/components/home/BoxTable.tsx
@@ -7,14 +7,18 @@ const BoxTable = async () => {
   return (
     <div className="grid grid-cols-3 gap-3 w-full px-6 mt-6 pb-20">
       {
-        userData?.openBoxHistories && userData?.openBoxHistories.map(item => (
+        userData?.openBoxHistories && userData?.openBoxHistories.map((item, index) => (
           <div className="box-card" key={item.boxNumber}>
             {
               item.open
                 ? <Image src="/assets/box-opened.webp" width={300} height={300} className="w-18 h-20" alt="box" />
                 : <Image src="/assets/box-open.webp" width={300} height={300} className="w-18 h-20" alt="box" />
             }
-            <p className="text-shadow-custom font-[860] text-[12px]">{item.title}</p>
+            <p className="text-shadow-custom font-[860] text-[12px]">
+              Box
+              {' '}
+              {index + 1}
+            </p>
             <p className="text-shadow-custom text-nowrap font-[590] text-[10px]">{item.description}</p>
             <ConfirmDialog boxNumber={item.boxNumber} isOpenBox={item.open} currentBox={userData.currentBox} />
           </div>

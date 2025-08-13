015706966ae25dae8b8635e91b0e69b283097168
Fink2005 <phans3806@gmail.com>
2025-08-13 21:17:53 +0700

    change image

--- src/components/home/BoxTable.tsx
+++ src/components/home/BoxTable.tsx
@@ -7,22 +7,24 @@ const BoxTable = async () => {
   const userData = await userRequest.userGetMe();
 
   return (
-    <div className="grid grid-cols-3 gap-3 w-full px-6 mt-6 pb-20">
+    <div className="grid grid-cols-3 gap-3 w-full px-6 mt-6 pb-20 max-">
       {
         userData?.openBoxHistories && userData?.openBoxHistories.map((item, index) => (
-          <div className="box-card" key={item.boxNumber}>
+          <div className="box-card max-h-[170px]" key={item.boxNumber}>
             {
               item.open
-                ? <Image src="/assets/box-opened.webp" width={300} height={300} className="w-18 h-20" alt="box" />
-                : <Image src="/assets/box-open.webp" width={300} height={300} className="w-18 h-20" alt="box" />
+                ? <Image src="/assets/box-opened.webp" width={300} height={300} className="w-26 h-26" alt="box" />
+                : <Image src="/assets/box-open.webp" width={300} height={300} className="w-26 h-26" alt="box" />
             }
-            <p className="text-shadow-custom font-[860] text-[12px]">
-              Box
-              {' '}
-              {index + 1}
-            </p>
-            <p className="text-shadow-custom text-nowrap font-[590] text-[10px]">{item.description.title}</p>
-            <ConfirmDialog boxNumber={item.boxNumber} isOpenBox={item.open} currentBox={userData.currentBox} />
+            <div className="flex flex-col items-center space-y-1 -translate-y-3">
+              <p className="text-shadow-custom font-[860] text-[12px]">
+                Box
+                {' '}
+                {index + 1}
+              </p>
+              <p className="text-shadow-custom text-nowrap font-[590] text-[10px]">{item.description.title}</p>
+              <ConfirmDialog boxNumber={item.boxNumber} isOpenBox={item.open} currentBox={userData.currentBox} />
+            </div>
           </div>
         ))
       }
